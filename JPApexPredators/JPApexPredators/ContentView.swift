41142acca55e7882ec2543009d4ff8fefe0322fa
Alvin Yan <apyan7852@gmail.com>
2025-01-19 14:57:02 -0800

    Update

--- JPApexPredators/JPApexPredators/ContentView.swift
+++ JPApexPredators/JPApexPredators/ContentView.swift
@@ -10,31 +10,98 @@ import SwiftUI
 struct ContentView: View {
     let predators = Predators()
     
+    @State var searchText = ""
+    @State var alphabetical = false
+    @State var currentSelection = APType.all
+    
+    var filteredDinos: [ApexPredator] {
+        predators.filter(by: currentSelection)
+        
+        predators.sort(by: alphabetical)
+        
+        return predators.search(for: searchText)
+    }
+    
     var body: some View {
-        // Scrolling and Lazy Loading is built into List
-        // Unlike ForEach
-        List(predators.apexPredators) { predator in
-            HStack {
-                // Dinosaur Image
-                Image(predator.image)
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 100, height: 100)
-                    .shadow(color: .white, radius: 1)
+        // Navigation Stack is used to stack screens on top of each other
+        // Similar to ZStack but with more 'power'
+        NavigationStack {
+            // Scrolling and Lazy Loading is built into List
+            // Unlike ForEach
+            List(filteredDinos) { predator in
+                // The right 'arrow'
+                NavigationLink {
+                    Image(predator.image)
+                        .resizable()
+                        .scaledToFit()
+                } label: {
+                    HStack {
+                        // Dinosaur Image
+                        Image(predator.image)
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 100, height: 100)
+                            .shadow(color: .white, radius: 1)
+                        
+                        VStack(alignment: .leading) {
+                            // Name
+                            Text(predator.name)
+                                .fontWeight(.bold)
+                            
+                            // Type
+                            Text(predator.type.rawValue.capitalized)
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                                .padding(.horizontal, 13)
+                                .padding(.vertical, 5)
+                                .background(predator.type.background)
+                                .clipShape(.capsule)
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Apex Predators")
+            .searchable(text: $searchText)
+            .autocorrectionDisabled()
+            // Uses subtle animation of expanding or shrinking list
+            .animation(.default, value: searchText)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button {
+                        // Add animation to the list, arrangements
+                        withAnimation {
+                            alphabetical.toggle()
+                        }
+                    } label: {
+//                        if (alphabetical) {
+//                            Image(systemName: "film")
+//                        } else {
+//                            Image(systemName: "textformat")
+//                        }
+                        
+                        // Ternary operator
+                        Image(systemName: alphabetical
+                              ? "film" : "textformat")
+                        .symbolEffect(.bounce, value: alphabetical)
+                    }
+                }
                 
-                VStack(alignment: .leading) {
-                    // Name
-                    Text(predator.name)
-                        .fontWeight(.bold)
-                    
-                    // Type
-                    Text(predator.type.rawValue.capitalized)
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .padding(.horizontal, 13)
-                        .padding(.vertical, 5)
-                        .background(predator.type.background)
-                        .clipShape(.capsule)
+                ToolbarItem(placement: .topBarTrailing) {
+                    Menu {
+                        // Title will be shown depending on device
+                        // helps the coder nonetheless
+                        // Animate the filtering
+                        Picker("Filter", selection: $currentSelection.animation()) {
+                            ForEach(APType.allCases) { type in
+                                Label(
+                                    type.rawValue.capitalized,
+                                    systemImage: type.icon
+                                )
+                            }
+                        }
+                    } label: {
+                        Image(systemName: "slider.horizontal.3")
+                    }
                 }
             }
         }
--- JPApexPredators/JPApexPredators/Predators.swift
+++ JPApexPredators/JPApexPredators/Predators.swift
@@ -11,6 +11,7 @@
 import Foundation
 
 class Predators {
+    var allApexPredators: [ApexPredator] = []
     var apexPredators: [ApexPredator] = []
     
     init() {
@@ -28,9 +29,42 @@ class Predators {
                 
                 // Snake Case -> Camel Case
                 decoder.keyDecodingStrategy = .convertFromSnakeCase
-                apexPredators = try decoder.decode([ApexPredator].self, from: data)
+                allApexPredators = try decoder.decode([ApexPredator].self, from: data)
+                apexPredators = allApexPredators
             } catch {
                 print("Error decoding JSON data: \(error)")
+            }
+        }
+    }
+    
+    func search(for searchTerm: String) -> [ApexPredator] {
+        if (searchTerm.isEmpty) {
+            return apexPredators
+        } else {
+            return apexPredators.filter { predator in
+                predator.name.localizedCaseInsensitiveContains(searchTerm)
+            }
+        }
+    }
+    
+    func sort(by alphabetical: Bool) {
+        apexPredators.sort { predator1, predator2 in
+            if (alphabetical) {
+                predator1.name < predator2.name
+            } else {
+                predator1.id < predator2.id
+            }
+        }
+    }
+    
+    func filter(by type: APType) {
+        // Sort sorts in place, this case the list of ApexPredator
+        // Filter filters into a temporary value
+        if (type == .all) {
+            apexPredators = allApexPredators
+        } else {
+            apexPredators = allApexPredators.filter { predator in
+                predator.type == type
             }
         }
     }
