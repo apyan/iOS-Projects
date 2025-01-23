02d44eb0d56771dd63b4250a6eba424606fad574
Alvin Yan <apyan7852@gmail.com>
2025-01-23 14:15:25 -0800

    New project

---
+++ BBQuotes/BBQuotes/BBQuotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  BBQuotesApp.swift
+//  BBQuotes
+//
+//  Created by Alvin on 1/23/25.
+//
+
+import SwiftUI
+
+@main
+struct BBQuotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
