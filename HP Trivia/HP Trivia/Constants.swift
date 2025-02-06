5b2bc73652820f4d4619d0c84f1a8e75b0284da0
Alvin Yan <apyan7852@gmail.com>
2025-02-05 19:23:35 -0800

    Set up

---
+++ HP Trivia/HP Trivia/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  HP Trivia
+//
+//  Created by Alvin on 2/5/25.
+//
+
+import Foundation
+
+enum Constants {
+    static let hpFont = "PartyLetPlain"
+}
---
+++ HP Trivia/HP Trivia/HP_TriviaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HP_TriviaApp.swift
+//  HP Trivia
+//
+//  Created by Alvin on 2/5/25.
+//
+
+import SwiftUI
+
+@main
+struct HP_TriviaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
